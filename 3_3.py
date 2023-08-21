bba3b331d8f36913dc0218e02a019f192bc2392c
Prem Pagare <prempagare27@gmail.com>
2023-08-22 00:04:34 +0530

    Update 3_3.py

--- 3_3.py
+++ 3_3.py
@@ -1,14 +1,16 @@
 correct_str= input("Enter Correct String: ").upper()
 content_str= input("Enter Contestant String: ").upper()
 count=0
-if len(content_str)<=10 and len(content_str)==len(correct_str):
+size=len(content_str)
+if size<=10 and size==len(correct_str):
     for i in correct_str:
         for j in content_str:
             if i==j:
                 count+=1
-if count==len(content_str):
+if count==size:
     print(content_str,"IS CORRECT")
-elif count==len(content_str)-2:
+elif count>=size-2:
     print(content_str,"IS ALMOST CORRECT")
+      
 else:
     print(content_str,"IS WRONG")
