f92873a91128d574cd710f7b58f26405bdbe7420
Prem Pagare <prempagare27@gmail.com>
2023-08-22 00:35:04 +0530

    Create 3_6.py

---
+++ 3_6.py
@@ -0,0 +1,9 @@
+string1=input("Enter the input string\n")
+str2=[]
+new_str=string1.split()
+for i in new_str:
+    if i==new_str[0]:
+        str2.append(i)
+        continue
+    str2.append(i.upper())
+print(' '.join(str2)) 
